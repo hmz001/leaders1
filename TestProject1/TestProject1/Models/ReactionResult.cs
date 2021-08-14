923082aece2611fda944b8ec301496e761e16276
Hamzeh Aljawawdeh <52409384+hmz001@users.noreply.github.com>
2021-08-14 16:03:53 +0200

    update

---
+++ TestProject1/TestProject1/Models/ReactionResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestProject1.Models
+{
+    public class ReactionResult
+    {
+        public bool Success { get; set; }
+        public string Result { get; set; }
+    }
+}
